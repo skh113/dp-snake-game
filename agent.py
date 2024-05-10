83d3cafeb561aeb7425a5558243ba78b7562d360
Keyvan Hosseini <skhprogramming@gmail.com>
2024-05-10 12:20:00 +0330

    Added global train function to agent

--- agent.py
+++ agent.py
@@ -11,7 +11,10 @@ LEARNING_RATE = 0.001
 
 class Agent:
     def __init__(self):
-        pass
+        self.games_number = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # after filling it self -> popleft()
 
     def get_state(self, game):
         pass
@@ -22,7 +25,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
@@ -30,7 +33,41 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+
+    while True:
+        # get old state
+        old_state = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(old_state)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        new_state = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(old_state, final_move, reward, new_state, done)
+
+        # remember
+        agent.remember(old_state, final_move, reward, new_state, done)
+
+        if done:
+            # train long memory, plot results
+            game.reset_game()
+            agent.games_number += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+
+            print(f"Game: {agent.games_number}, Score: {score}, Record: {record}")
+            # plot
 
 
 if __name__ == "__main__":
